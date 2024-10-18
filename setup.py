7f15f197a9c2bf460ad8c8aa9bd4eb31b272e78f
Vallion <151787330+VallionXD@users.noreply.github.com>
2024-10-17 21:11:58 -0400

    Update #5 | Fix Setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ws_hash',
+    name='ws-hash',
     version='1.1.0',
     packages=find_packages(),
-    description='A simple hash library implemented in Python, featuring both 256-bit and 512-bit hash functions.',
-    long_description=open('README.md').read(),
+    description='A simple yet hash library implemented in Python, featuring both 256-bit and 512-bit hash functions.',
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='VallionXD',
-    author_email='',
+    author_email='your.email@example.com',
     url='https://github.com/VallionXD/ws-hash/',
     classifiers=[
         'Programming Language :: Python :: 3',
